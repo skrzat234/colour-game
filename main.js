124f1f5636e9dbbd5b30f64a604f85035d86b15f
Marek <151378137+skrzat234@users.noreply.github.com>
2025-01-26 01:10:09 +0100

    main variables, mouse click support
    added main variables and basic support for mouse clicks - selecting a color and assigning this color to the appropriate place and colouring the appropriate box

--- main.js
+++ main.js
@@ -3,9 +3,14 @@ let guessColors = ["white", "white", "white", "white", "white"];
 let availableColors = ["red", "green", "blue", "yellow", "purple"];
 let cellSize = 60;
 let numColors = 5;
+let wybraneKolory = [null, null, null, null, null];
+let kliknietyKolor = null;
+
 
 function setup() {
   createCanvas(600, 400);
+  sekwencja = shuffle([...availableColors]).slice(0, numColors);
+  console.log("losowa sekwencja:", sekwencja); //F12
 }
 
 function draw() {
@@ -20,10 +25,10 @@ function draw() {
     rect(100 + i * cellSize, 50, cellSize, cellSize);
   }
 
-  // kolory odgadywane
+  // Kolory odgadywane
   text("kolory odgadywane", 20, 150);
   for (let i = 0; i < numColors; i++) {
-    fill(guessColors[i]);
+    fill(wybraneKolory[i] || "white"); // Domyślny kolor to 'white', gdy null
     rect(100 + i * cellSize, 170, cellSize, cellSize);
     stroke(0);
     noFill();
@@ -35,5 +40,68 @@ function draw() {
   for (let i = 0; i < availableColors.length; i++) {
     fill(availableColors[i]);
     rect(100 + i * cellSize, 320, cellSize, cellSize);
+  }
+}
+
+
+
+
+// Funkcja do obsługi kliknięcia na palecie koloróww
+function wybierzKolor(kolor) {
+  kliknietyKolor = kolor;
+  console.log("Wybrano kolor:", kliknietyKolor);
+}
+
+// Funkcja do obsługi kliknięcia na białe miejsce na planszy
+function ustawKolor(idBox) {
+  if (kliknietyKolor === null) {
+    console.log("Najpierw wybierz kolor z palety!");
+    return;
+  }
+
+  if (wybraneKolory[idBox] !== null) {
+    console.log("To miejsce jest już zajęte!");
+    return;
+  }
+
+  // Przypisanie wybranego koloru do odpowiedniego miejsca
+  wybraneKolory[idBox] = kliknietyKolor;
+  console.log("Ustawiono kolor:", kliknietyKolor, "na pozycji:", idBox);
+
+  // Usunięcie koloru z palety
+  usunKolorZPalety(kliknietyKolor);
+
+  // Reset klikniętego koloru
+  kliknietyKolor = null;
+}
+
+// Funkcja do usunięcia koloru z palety
+function usunKolorZPalety(kolor) {
+  const index = availableColors.indexOf(kolor);
+  if (index !== -1) {
+    availableColors.splice(index, 1); // Usuwa kolor z palety
+    console.log("Usunięto kolor z palety:", kolor);
+  }
+}
+
+
+
+function mousePressed() {
+  // Sprawdzanie kliknięcia na paletę kolorów
+  for (let i = 0; i < availableColors.length; i++) {
+    let x = 100 + i * cellSize;
+    let y = 320;
+    if (mouseX > x && mouseX < x + cellSize && mouseY > y && mouseY < y + cellSize) {
+      wybierzKolor(availableColors[i]);
+    }
+  }
+
+  // Sprawdzanie kliknięcia na białe pola
+  for (let i = 0; i < numColors; i++) {
+    let x = 100 + i * cellSize;
+    let y = 170;
+    if (mouseX > x && mouseX < x + cellSize && mouseY > y && mouseY < y + cellSize) {
+      ustawKolor(i);
+    }
   }
 }
